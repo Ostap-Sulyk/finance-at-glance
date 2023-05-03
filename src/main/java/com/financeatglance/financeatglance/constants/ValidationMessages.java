a2ff9f1ad2eddf19d81c8dc2bc862f3e8a47b3c2
Ostap <ostapsulyk@gmail.com>
2023-05-03 06:49:25 -0400

    Implemented Validation to encourage strong password from users

---
+++ src/main/java/com/financeatglance/financeatglance/constants/ValidationMessages.java
@@ -0,0 +1,15 @@
+package com.financeatglance.financeatglance.constants;
+
+public class ValidationMessages {
+    // password validation
+    public static final String TWO_UPPERCASE_LETTERS = "Password must contain at least two uppercase letters";
+    public static final String ONE_SPECIAL_CHARACTER = "Password must contain at least one special character (@#$%^&+=!)";
+    public static final String TWO_DIGITS = "Password must contain at least two digits";
+    public static final String THREE_LOWERCASE_LETTERS = "Password must contain at least three lowercase letters";
+    public static final String PASSWORD_TOO_SHORT = "Password must be at least 8 characters long";
+    public static final String BLANK_FIELD = "Field cannot be blank";
+    public static final String CONTAIN_ONLY_LETTERS = "Field must contain only letters";
+    public static final String INVALID_EMAIL = "Email should be valid";
+    public static final String EMAIL_ALREADY_EXIST = "Email already taken";
+    public static final String POSITIVE = "Amount must be positive";
+}
